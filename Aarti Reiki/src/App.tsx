22f2a9fa09a50b4fdf33b47ce93f8543297895f4
AlexStraton <alexandra.straton@curaleafint.com>
2025-03-01 12:49:44 +0000

    Installed and confisgured Tailwind

---
+++ Aarti Reiki/src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+
+      <p className="text-5xl font-bold text-center text-white-800 my-8">
+       Reiki by Aarti
+      </p>
+      </div>
+    </>
+  )
+}
+
+export default App
